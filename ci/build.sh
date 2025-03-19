85da6f335cd58cb7025c6075008c58c60399eceb
Jimmy Christensen <lithorus@gmail.com>
2025-03-19 22:12:59 +0100

    Update

--- ci/build.sh
+++ ci/build.sh
@@ -10,5 +10,7 @@ python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install build
 
-hatch version "$(git describe --tags --abbrev=1 | sed 's/-/+/')"
+git describe --tags --abbrev=1
+VERSION="$(git describe --tags --abbrev=1 | sed 's/-/+/')"
+hatch version "${VERSION}"
 python3 -m build
