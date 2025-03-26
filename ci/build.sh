55092e7ba6129a90a1981f651fadf7c066bdcca3
Jimmy Christensen <lithorus@gmail.com>
2025-03-26 23:27:06 +0100

    Update

--- ci/build.sh
+++ ci/build.sh
@@ -12,7 +12,7 @@ python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install build hatchling
 
-git describe --tags --abbrev=1
-VERSION="$(git describe --tags --abbrev=1 | sed 's/-/+/')"
-hatchling version "${VERSION}"
+# git describe --tags --abbrev=1
+# VERSION="$(git describe --tags --abbrev=1 | sed 's/-/+/')"
+# hatchling version "${VERSION}"
 python3 -m build
